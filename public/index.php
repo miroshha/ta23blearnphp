fa489da148a23413d5cf3bb8c88988493b467724
Miron Gavva <50610197031@tptlive.ee>
2025-10-03 09:44:35 +0300

    routing added

--- public/index.php
+++ public/index.php
@@ -11,7 +11,6 @@ function dump(...$args) {
 }
 
 spl_autoload_register(function($class) {
-    dump($class);
     $class = substr($class, 4);
     require_once __DIR__. "/../src/$class.php";
 });
@@ -19,42 +18,36 @@ spl_autoload_register(function($class) {
 // require_once __DIR__. '/../src/Router.php';
 // require_once __DIR__. '/../src/DB.php';
 
-use App\Controllers\PublicController as PC;
+use App\Router;
 
-$router = new App\Router();
-dump($router);
-$db = new App\DB();
-dump($db);
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-$controller = new PC();
-dump($controller);
+Router::addRoute('/', function() {
+    $posts = [
+        ['title' => "World news 1", "published" => '26.09.2025', 'author' => "Miron", 'body' => "Some kind of news1"],
+        ['title' => "World news 1", "published" => '27.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news2"],
+        ['title' => "World news 3", "published" => '28.09.2025', 'author' => "Miron", 'body' => "Some kind of news3"],
+        ['title' => "World news 4", "published" => '29.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news4"],
+        ['title' => "World news 5", "published" => '30.09.2025', 'author' => "Miron", 'body' => "Some kind of news5"],
+    ];
+    include __DIR__ . '/../views/index.php';
+});
 
-// switch ($_SERVER['REQUEST_URI']) {
-//     case '/': 
-//         $posts = [
-//             ['title' => "World news 1", "published" => '26.09.2025', 'author' => "Miron", 'body' => "Some kind of news1"],
-//             ['title' => "World news 1", "published" => '27.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news2"],
-//             ['title' => "World news 3", "published" => '28.09.2025', 'author' => "Miron", 'body' => "Some kind of news3"],
-//             ['title' => "World news 4", "published" => '29.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news4"],
-//             ['title' => "World news 5", "published" => '30.09.2025', 'author' => "Miron", 'body' => "Some kind of news5"],
-//         ];
-//         include __DIR__ . '/../views/index.php';
-//         break;
-//     case '/us':
-//         $posts = [
-//             ['title' => "U.S news 1", "published" => '26.09.2025', 'author' => "Miron", 'body' => "Some kind of news1"],
-//             ['title' => "U.S news 2", "published" => '27.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news2"],
-//             ['title' => "U.S news 3", "published" => '28.09.2025', 'author' => "Miron", 'body' => "Some kind of news3"],
-//             ['title' => "U.S news 4", "published" => '29.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news4"],
-//             ['title' => "U.S news 5", "published" => '30.09.2025', 'author' => "Miron", 'body' => "Some kind of news5"],
-//         ];
-//         include __DIR__ . '/../views/us.php';
-//         break;
-//     default: 
-//         http_response_code(404);
-//         echo '404 page not found';
-// }
+
+Router::addRoute('/us', function() {
+    $posts = [
+        ['title' => "U.S news 1", "published" => '26.09.2025', 'author' => "Miron", 'body' => "Some kind of news1"],
+        ['title' => "U.S news 2", "published" => '27.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news2"],
+        ['title' => "U.S news 3", "published" => '28.09.2025', 'author' => "Miron", 'body' => "Some kind of news3"],
+        ['title' => "U.S news 4", "published" => '29.09.2025', 'author' => "Kaspar", 'body' => "Some kind of news4"],
+        ['title' => "U.S news 5", "published" => '30.09.2025', 'author' => "Miron", 'body' => "Some kind of news5"],
+    ];
+    include __DIR__ . '/../views/us.php';
+});
+
+$router = new App\Router($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if ($match) {
+    call_user_func($match['action']);
+} else {
+    http_response_code(404);
+    echo '<img src="https://http.cat/404"></img>';
+}
