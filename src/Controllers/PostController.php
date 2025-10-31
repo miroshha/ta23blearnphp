aeed425c92fb31ca62fbd8eaa8f8362b058ba7ab
miroshha <miron.gavva@tptlive.ee>
2025-10-31 17:21:42 +0200

    composer added

--- src/Controllers/PostController.php
+++ src/Controllers/PostController.php
@@ -3,22 +3,30 @@
 namespace App\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 
 class PostController
 {
+    public function __construct()
+    {
+        if(!auth()){
+            redirect('/login');
+            die;
+        }
+    }
+
     public function index()
     {
         $posts = Post::all();
+        dump($posts);
         view('posts/index', compact('posts'));
     }
 
-    public function create()
-    {
+    public function create() {
         view('posts/create');
     }
 
-    public function store()
-    {
+    public function store() {
         $post = new Post();
         $post->title = $_POST['title'];
         $post->body = $_POST['body'];
@@ -26,21 +34,17 @@ class PostController
         redirect('/posts');
     }
 
-    public function view()
-    {
+    public function view() {
         $post = Post::find($_GET['id']);
-
         view('posts/view', compact('post'));
     }
 
-    public function edit()
-    {
+    public function edit(){
         $post = Post::find($_GET['id']);
         view('posts/edit', compact('post'));
     }
 
-    public function update()
-    {
+    public function update() {
         $post = Post::find($_GET['id']);
         $post->title = $_POST['title'];
         $post->body = $_POST['body'];
@@ -48,7 +52,7 @@ class PostController
         redirect('/posts');
     }
 
-    public function destroy() {
+    public function destroy(){
         $post = Post::find($_GET['id']);
         $post->delete();
         redirect('/posts');
